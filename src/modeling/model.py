ca48812c91d0b8c21e9f08d2d0e610ae3e648121
johanfal <fredrikalvsaker@gmail.com>
2020-06-02 15:27:48 +0200

    added clean template file

---
+++ src/modeling/model.py
@@ -0,0 +1,36 @@
+import pickle
+import sys, os
+
+import numpy as np
+import pandas as pd
+
+from src.funcs import memory as mem
+from src.modeling import helper_funcs as fnc
+from tensorflow import keras
+
+def create():
+    """Implement your creation function here."""
+    sys.exit('Not implemented.')
+
+    # return model
+
+def train():
+    """Implement your training function here."""
+    sys.exit('Not implemented.')
+
+    # return model, history
+
+def test():
+    """Implement your testing function here."""
+    sys.exit('Not implemented.')
+
+    # return performance
+
+def visualize():
+    """Implement your visualization function here."""
+    sys.exit('Not implemented.')
+
+    # return
+
+if __name__ == '__main__':
+    sys.exit(f'Run from manage.py, not {os.path.basename(__file__)}.')
