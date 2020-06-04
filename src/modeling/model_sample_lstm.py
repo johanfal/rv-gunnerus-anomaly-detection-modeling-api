560ba1dee45c4e5d5f8d27eba01a75742ea009ad
johanfal <fredrikalvsaker@gmail.com>
2020-06-04 16:52:15 +0200

    added visualization functionality

--- src/modeling/model_sample_lstm.py
+++ src/modeling/model_sample_lstm.py
@@ -1,11 +1,13 @@
-import pickle
+import os, pickle, sys
 
 import numpy as np
 import pandas as pd
-
-from src.funcs import memory as mem
-from src.modeling import helper_funcs as fnc
 from tensorflow import keras
+
+from src.api import file_management as filemag
+from src.api import memory as mem
+from src.api import modeling_funcs as mfnc
+from src.api import plotting_funcs as pfnc
 
 def create(input_shape:tuple,verbose:bool=True,**parameters) -> 'keras.model':
     """DESCRIPTION. The function takes a variable number of keyword arguments,
@@ -66,7 +68,7 @@ def train(
 
 def test(
             model:'keras.model',
-            history:list,
+            history:dict,
             df_test:pd.DataFrame,
             X_test:np.ndarray,
             threshold_pct:float,
@@ -90,105 +92,86 @@ def test(
     df_test = df_test[timesteps:]
 
     # Merge original and predicted values in a single dataframe
-    df_hat = fnc.get_df_hat(df_test,y_hat,prediction_cols=prediction_cols)
+    df_hat = mfnc.get_df_hat(df_test,y_hat,prediction_cols=prediction_cols)
 
     # Inverse transform df_hat and df_test back to original magnitudes:
-    df_hat_inv = fnc.inverse_transform_dataframe(df_hat, pred_scaler)
-    df_test_inv = fnc.inverse_transform_dataframe(df_test, test_scaler)
+    df_hat_inv = mfnc.inverse_transform_dataframe(df_hat, pred_scaler)
+    df_test_inv = mfnc.inverse_transform_dataframe(df_test, test_scaler)
 
     # Filter prediction columns:
     df_hat_filtered = df_hat_inv.filter(prediction_cols)
     df_test_filtered = df_test_inv.filter(prediction_cols)
 
     # Calculate absolute error for each predicted timestep:
-    absolute_error = fnc.get_absolute_error(df_hat_filtered, df_test_filtered)
+    absolute_error = mfnc.get_absolute_error(df_hat_filtered,df_test_filtered)
 
+    # Calculate thresholds based on an anomaly distribution percentage:
+    # (If threshold_pct is 70 %, the threshold value will be the minimum of
+    # the 30 % highest values. Threshold_pct can either be passed as a single,
+    # uniform value, or as a list of percentages for each predicted column.)
+    # thresholds = mfnc.get_thresholds(absolute_error, threshold_pct)
+    thresholds = mfnc.get_thresholds(absolute_error, threshold_pct)
 
+    # Calculate mean absolute error (MAE) for each predicted column:
+    mae = mfnc.get_mae(absolute_error)
+    # Calculate root mean square error (RMSE) for each predicted column:
+    # rmse = mfnc.get_rmse(df_hat_filtered, df_test_filtered)
 
+    performance = mfnc.get_performance(
+                                    df_pred_filtered=df_hat_filtered,
+                                    df_real_filtered=df_test_filtered,
+                                    absolute_error=absolute_error,
+                                    thresholds=thresholds,
+                                    anomaly_neighborhood=anomaly_neighborhood
+                                )
 
-    threshold_pcts = [
-        # 90,
-        # 91,
-        # 92,
-        # 93,
-        # 94,
-        95,
-        96,
-        97,
-        # 97.10,
-        # 97.20,
-        # 97.25,
-        # 97.30,
-        # 97.40,
-        # 97.50,
-        # 97.60,
-        # 97.70,
-        # 97.75,
-        # 97.80,
-        # 97.90,
-        98,
-        # 98.25,
-        # 98.5,
-        # 98.75,
-        99,
-    ]
-    neighbors = [
-        # 5,
-        # 10,
-        # 12,
-        # 14,
-        15,
-        # 16,
-        # 17,
-        # 18,
-        # 19,
-        20,
-        25,
-        30,
-        35,
-        40
-    ]
-    result = {}
-    for pct in threshold_pcts:
-        for neighbor in neighbors:
-            print(f"pct: {pct}. neighborhood: {neighbor}.")
-
-            # Calculate thresholds based on an anomaly distribution percentage:
-            # (If threshold_pct is 70 %, the threshold value will be the minimum of
-            # the 30 % highest values. Threshold_pct can either be passed as a single,
-            # uniform value, or as a list of percentages for each predicted column.)
-            # thresholds = fnc.get_thresholds(absolute_error, threshold_pct)
-            thresholds = fnc.get_thresholds(absolute_error, pct)
-
-            # Calculate mean absolute error (MAE) for each predicted column:
-            mae = fnc.get_mae(absolute_error)
-            # Calculate root mean square error (RMSE) for each predicted column:
-            # rmse = fnc.get_rmse(df_hat_filtered, df_test_filtered)
-            performance = fnc.get_performance(
-                                            df_pred_filtered=df_hat_filtered,
-                                            df_real_filtered=df_test_filtered,
-                                            absolute_error=absolute_error,
-                                            thresholds=thresholds,
-                                            # anomaly_neighborhood=anomaly_neighborhood
-                                            anomaly_neighborhood=neighbor
-                                        )
-            result[f"pct-{pct}_neg-{neighbor}"] = [performance,absolute_error,thresholds]
-    mem.store(result,file_prefix='faulty_testing_results')
-    import sys
-    sys.exit("Done.")
-    # Necessary parameters:
-    # col, absolute_error, thresholds, df_hat_filtered, df_test_filtered,
     return performance, absolute_error, thresholds
 
-def visualize(performance:dict,history:list, **kwargs) -> None:
+def visualize(performance:dict,history:dict, **kwargs) -> None:
+    """DESCRIPTION."""
 
+    # Retrieving parameters:
     absolute_error = kwargs['absolute_error']
     thresholds = kwargs['thresholds']
+    units = kwargs['units']
+    exclude_plots=True
+    # Training history:
+    pfnc.get_historyplt(history)
 
+    # Plot results for each predicted state:
+    for key in performance:
 
-    for key, df in performance.items():
-        print("hello!")
-    return
+        # # Predicted state values:
+        pfnc.get_predplt(
+                            real=performance[key].real,
+                            pred=performance[key].pred,
+                            signal=key,
+                            unit=units[key]
+                    )
+
+        # Loss distribution as 2x2 histograms with kernal density plots (KDE):
+        pfnc.get_distplt(
+                            signal=key,
+                            dist=performance[key].loss,
+                            threshold=thresholds[key],
+                            unit=units[key]
+                        )
+
+        # State value loss and threshold:
+        pfnc.loss_threshold(
+                                signal=key,
+                                loss=performance[key].loss,
+                                threshold=thresholds[key],
+                                unit=units[key]
+                            )
+
+        # Plot anomalies:
+        pfnc.get_anomalyplt(
+                                signal=key,
+                                real=performance[key].real,
+                                anoms=performance[key].anom,
+                                unit=units[key]
+                        )
 
 if __name__ == '__main__':
     import sys, os
